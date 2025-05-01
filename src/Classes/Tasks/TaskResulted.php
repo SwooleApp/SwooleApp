b7f5680cfe051ce587e98ab419e1c7c532353d05
SidorkinAlex <palach.orl@mail.ru>
2025-05-01 15:26:28 +0300

    fixed TaskException building

--- src/Classes/Tasks/TaskResulted.php
+++ src/Classes/Tasks/TaskResulted.php
@@ -18,10 +18,13 @@ class TaskResulted
      * @return mixed
      * @throws TaskException
      */
-    public function getResult(): mixed
-    {
+    public function getResult(): mixed {
         if (!$this->success) {
-            throw new TaskException($this->result);
+            throw new TaskException(
+                $this->result === 'Task execution failed'
+                    ? $this->result
+                    : "Task execution failed: " . var_export($this->result, true)
+            );
         }
         return $this->result;
     }
