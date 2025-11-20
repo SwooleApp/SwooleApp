a2fb189f621b3bdd6e78f19a5ea41ed6df87f892
SidorkinAlex <palach.orl@mail.ru>
2025-11-20 12:45:16 +0300

    add middleware in controller

--- src/Classes/Controllers/AbstractController.php
+++ src/Classes/Controllers/AbstractController.php
@@ -3,6 +3,10 @@
 namespace Sidalex\SwooleApp\Classes\Controllers;
 
 use Sidalex\SwooleApp\Application;
+use Sidalex\SwooleApp\Classes\Middleware\ConfigurableMiddlewareInterface;
+use Sidalex\SwooleApp\Classes\Middleware\Middleware;
+use Sidalex\SwooleApp\Classes\Middleware\MiddlewareInterface;
+use Sidalex\SwooleApp\Classes\Utils\Utilities;
 use Swoole\Http\Server;
 
 abstract class AbstractController implements ControllerInterface
@@ -15,6 +19,10 @@ abstract class AbstractController implements ControllerInterface
     protected array $uri_params;
     protected Application $application;
     protected Server $server;
+    /**
+     * @var MiddlewareInterface[]
+     */
+    protected array $middlewares = [];
 
     public function __construct(\Swoole\Http\Request $request, \Swoole\Http\Response $response, array $uri_params = [])
     {
@@ -29,6 +37,82 @@ abstract class AbstractController implements ControllerInterface
     {
         $this->application = $application;
         $this->server = $server;
+    }
+    /**
+     * @return MiddlewareInterface[]
+     */
+    public function getMiddlewares(): array
+    {
+        return $this->middlewares;
+    }
+
+    /**
+     * Извлечение Middleware из атрибутов контроллера
+     */
+    protected function resolveMiddlewares(): void
+    {
+        $reflection = new \ReflectionClass($this);
+        $attributes = $reflection->getAttributes();
+
+        foreach ($attributes as $attribute) {
+            $attributeInstance = $attribute->newInstance();
+
+            if ($attributeInstance instanceof Middleware) {
+                $this->middlewares[] = [
+                    'class' => $attributeInstance->middlewareClass,
+                    'options' => $attributeInstance->options
+                ];
+            }
+        }
+    }
+
+    public function executeWithMiddlewares(): \Swoole\Http\Response
+    {
+        $middlewares = $this->getMiddlewares();
+
+        if (empty($middlewares)) {
+            return $this->execute();
+        }
+
+        $runner = function ($request, $response) use (&$runner, &$middlewares) {
+            if (empty($middlewares)) {
+                return $this->execute();
+            }
+
+            $middlewareConfig = array_shift($middlewares);
+            $middleware = $this->createMiddleware($middlewareConfig);
+
+            return $middleware->process(
+                $request,
+                $response,
+                $this->application,
+                function ($req, $resp) use ($runner) {
+                    return $runner($req, $resp);
+                }
+            );
+        };
+
+        return $runner($this->request, $this->response);
+    }
+
+    /**
+     * Фабрика для создания объектов Middleware
+     */
+    protected function createMiddleware(array $config): MiddlewareInterface
+    {
+        $className = $config['class'];
+        $options = $config['options'] ?? [];
+
+        if (!Utilities::classImplementInterface($className, MiddlewareInterface::class)) {
+            throw new \InvalidArgumentException("Middleware class {$className} must implement MiddlewareInterface");
+        }
+
+        // Для Middleware с поддержкой конфигурации
+        if (Utilities::classImplementInterface($className, ConfigurableMiddlewareInterface::class)) {
+            return new $className($options);
+        }
+
+        return new $className();
     }
 
 }
--- src/Classes/Controllers/NotFoundController.php
+++ src/Classes/Controllers/NotFoundController.php
@@ -3,6 +3,10 @@
 namespace Sidalex\SwooleApp\Classes\Controllers;
 
 use Sidalex\SwooleApp\Application;
+use Sidalex\SwooleApp\Classes\Middleware\ConfigurableMiddlewareInterface;
+use Sidalex\SwooleApp\Classes\Middleware\Middleware;
+use Sidalex\SwooleApp\Classes\Middleware\MiddlewareInterface;
+use Sidalex\SwooleApp\Classes\Utils\Utilities;
 use Swoole\Http\Server;
 
 class NotFoundController implements ControllerInterface
@@ -15,6 +19,7 @@ class NotFoundController implements ControllerInterface
      */
     // @phpstan-ignore-next-line
     private array $uri_params;
+    private array $middlewares = [];
 
     public function __construct(\Swoole\Http\Request $request, \Swoole\Http\Response $response, array $uri_params = [])
     {
@@ -35,6 +40,83 @@ class NotFoundController implements ControllerInterface
         ));
         //todo when creating logs , then add to log request and $uri_params from dev mode
         return $this->responce;
+    }
+
+    /**
+     * @return MiddlewareInterface[]
+     */
+    public function getMiddlewares(): array
+    {
+        return $this->middlewares;
+    }
+
+    /**
+     * Извлечение Middleware из атрибутов контроллера
+     */
+    protected function resolveMiddlewares(): void
+    {
+        $reflection = new \ReflectionClass($this);
+        $attributes = $reflection->getAttributes();
+
+        foreach ($attributes as $attribute) {
+            $attributeInstance = $attribute->newInstance();
+
+            if ($attributeInstance instanceof Middleware) {
+                $this->middlewares[] = [
+                    'class' => $attributeInstance->middlewareClass,
+                    'options' => $attributeInstance->options
+                ];
+            }
+        }
+    }
+
+    public function executeWithMiddlewares(): \Swoole\Http\Response
+    {
+        $middlewares = $this->getMiddlewares();
+
+        if (empty($middlewares)) {
+            return $this->execute();
+        }
+
+        $runner = function ($request, $response) use (&$runner, &$middlewares) {
+            if (empty($middlewares)) {
+                return $this->execute();
+            }
+
+            $middlewareConfig = array_shift($middlewares);
+            $middleware = $this->createMiddleware($middlewareConfig);
+
+            return $middleware->process(
+                $request,
+                $response,
+                $this->application,
+                function ($req, $resp) use ($runner) {
+                    return $runner($req, $resp);
+                }
+            );
+        };
+
+        return $runner($this->request, $this->response);
+    }
+
+    /**
+     * Фабрика для создания объектов Middleware
+     */
+    protected function createMiddleware(array $config): MiddlewareInterface
+    {
+        $className = $config['class'];
+        $options = $config['options'] ?? [];
+
+        if (!Utilities::classImplementInterface($className, MiddlewareInterface::class)) {
+            throw new \InvalidArgumentException("Middleware class {$className} must implement MiddlewareInterface");
+        }
+
+        // Для Middleware с поддержкой конфигурации
+        if (Utilities::classImplementInterface($className, ConfigurableMiddlewareInterface::class)) {
+            return new $className($options);
+        }
+
+        return new $className();
     }
 
     public function setApplication(Application $application, Server $server): void
