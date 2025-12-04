5ecfeb7106f8dc91e3a989367120f09b5c9bc236
SidorkinAlex <palach.orl@mail.ru>
2025-12-04 08:59:37 +0300

    add fix types in middleware

--- src/Classes/Middleware/GlobalMiddleware.php
+++ src/Classes/Middleware/GlobalMiddleware.php
@@ -7,6 +7,10 @@ use Attribute;
 #[Attribute(Attribute::TARGET_CLASS)]
 class GlobalMiddleware
 {
+    /**
+     * @param string $middlewareClass
+     * @param array<mixed> $options
+     */
     public function __construct(
         public string $middlewareClass,
         public array  $options = []
